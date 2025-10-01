5e2e5336c103faaf2bb2e6856e5512d0e6a9e989
MichaelStarshynov <m.starshynov@gmail.com>
2025-10-01 13:42:21 +0200

    Меняю фотку

--- JavaScript.js
+++ JavaScript.js
@@ -104,7 +104,7 @@ const pages = {
   },
   Магазин: {
     title: `Купити атрибутику`,
-    text: `Атрибутику Шахтаря можно купити <a href="https://shop.shakhtar.com/ru/" target="_blank">тут</a>.<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRsxQjeSy2AG37trL2PTyXWF-4GIMO7mvkXzA&s" style="max-width:300px;">`
+    text: `Атрибутику Шахтаря можно купити <a href="https://shop.shakhtar.com/ru/" target="_blank">тут</a>.<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQvusJ_h7AcI6u3vAW2IKSlkF9CKU_9FtTHOA&s" style="max-width:300px;">`
   },
   Телеграм: {
     title: `Телеграм-канал`,
